b0d9f114ea960a853c822efcd48cd1b18c9df175
ayumiMari <kitsusemariana@gmail.com>
2024-06-05 13:22:32 -0300

    flappy
    destruicao

--- Assets/Scripts/Obstaculo.cs
+++ Assets/Scripts/Obstaculo.cs
@@ -1,20 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Accessibility;
 
 public class Obstaculo : MonoBehaviour
 {
-    [SerializeField]
+    [SerializeField] //variavel que aparece na interface, "editar pelo jogo"
     private float velocidade = 0.6f;
     // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float variacaoY;
+    private void Awake() //void não devolve/retorna nada
     {
-        
+        this.transform.Translate(Vector3.up * Random.Range(-variacaoY, variacaoY));
     }
 
     // Update is called once per frame
     void Update()
     {
         this.transform.Translate(Vector3.left * velocidade * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        this.Destruir();
+    }
+    void Destruir()
+    {
+        GameObject.Destroy(this.gameObject);
     }
 }
